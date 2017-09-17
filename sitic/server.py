fe18916af5553b86967339b57fa1b7e55bb8eec4
Jose Marente <jose.marente@crononauta.com>
2017-09-17 11:50:14 +0200

    Simplify http server and fixes query string parameters issue

--- sitic/server.py
+++ sitic/server.py
@@ -4,41 +4,16 @@ import sys
 import os
 import posixpath
 import argparse
+from urllib.parse import urlparse, parse_qs
 from multiprocessing import Process
 
-from six.moves import SimpleHTTPServer, socketserver, BaseHTTPServer
+from six.moves import SimpleHTTPServer, socketserver
 from six.moves.urllib import parse
 
 from sitic.generator import Generator
 from sitic.watcher import Watcher
 from sitic.config import config
 from sitic.logging import logger
-
-INDEXFILE = 'index.html'
-
-
-class HttpServer(SimpleHTTPServer.HTTPServer):
-
-    def __init__(self, base_path, *args, **kwargs):
-        SimpleHTTPServer.HTTPServer.__init__(self, *args, **kwargs)
-        self.RequestHandlerClass.base_path = base_path
-
-
-class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-
-    def translate_path(self, path):
-        dth = posixpath.normpath(parse.unquote(path))
-        words = path.split('/')
-        words = filter(None, words)
-        path = self.base_path
-        for word in words:
-            drive, word = os.path.splitdrive(word)
-            head, word = os.path.split(word)
-            if word in (os.curdir, os.pardir):
-                continue
-            path = os.path.join(path, word)
-        return path
-
 
 class Server(object):
 
@@ -60,9 +35,13 @@ class Server(object):
 
     def start_server(self):
 
-        Handler = HttpHandler
+        config.public_path
+        self.port
 
-        httpd = HttpServer(config.public_path, ("", self.port), HttpHandler)
+        os.chdir(config.public_path)
+
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = socketserver.TCPServer(("", self.port), Handler)
 
         logger.info("Serving at: localhost:{}".format(self.port))
 
