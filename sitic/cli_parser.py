f7a1fad50b92dfc679dbab6a8f22aa022dc2ecf7
Jose Marente <jose.marente@crononauta.com>
2017-09-23 16:43:58 +0200

    improves cli help

--- sitic/cli_parser.py
+++ sitic/cli_parser.py
@@ -16,15 +16,15 @@ from sitic.commands.new_site_command import NewSiteCommand
 
 @click.group(invoke_without_command=True)
 @click.pass_context
-@click.option('--config', default='sitic.yml', type=click.Path())
-@click.option('--build_draft', is_flag=True, default=False, type=bool)
-@click.option('--build_future', is_flag=True, default=False, type=bool)
-@click.option('--build_expired', is_flag=True, default=False, type=bool)
-@click.option('--disable_sitemap', is_flag=True, default=False, type=bool)
-@click.option('--disable_rss', is_flag=True, default=False, type=bool)
-@click.option('--disable_search', is_flag=True, default=False, type=bool)
-@click.option('--remove_expired', is_flag=True, default=False, type=bool)
-@click.option('--clean_destination', is_flag=True, default=False, type=bool)
+@click.option('--config', default='sitic.yml', type=click.Path(), help="Filesystem path to config file")
+@click.option('--build_draft', is_flag=True, default=False, type=bool, help="Include content marked as draft")
+@click.option('--build_future', is_flag=True, default=False, type=bool, help="Include content with publication_date in the future")
+@click.option('--build_expired', is_flag=True, default=False, type=bool, help="Include expired content")
+@click.option('--disable_sitemap', is_flag=True, default=False, type=bool, help="Do not generate Sitemap")
+@click.option('--disable_rss', is_flag=True, default=False, type=bool, help="Do not generate RSS")
+@click.option('--disable_search', is_flag=True, default=False, type=bool, help="Do not generate search indexer")
+@click.option('--remove_expired', is_flag=True, default=False, type=bool, help="Remove already published content in destination folder")
+@click.option('--clean_destination', is_flag=True, default=False, type=bool, help="Completelty remove destination folder before generation")
 def cli(ctx, config, build_draft, build_future, build_expired, disable_sitemap, disable_rss, disable_search, remove_expired, clean_destination):
     """
     Sitic is a Static Site Generator built in Python.
@@ -36,16 +36,19 @@ def cli(ctx, config, build_draft, build_future, build_expired, disable_sitemap, 
 
 
 @cli.command()
-@click.option('--config', default='sitic.yml', type=click.Path(exists=True))
-@click.option('--build_draft', is_flag=True, default=False, type=bool)
-@click.option('--build_future', is_flag=True, default=False, type=bool)
-@click.option('--build_expired', is_flag=True, default=False, type=bool)
-@click.option('--disable_sitemap', is_flag=True, default=False, type=bool)
-@click.option('--disable_rss', is_flag=True, default=False, type=bool)
-@click.option('--disable_search', is_flag=True, default=False, type=bool)
-@click.option('--remove_expired', is_flag=True, default=False, type=bool)
-@click.option('--clean_destination', is_flag=True, default=False, type=bool)
+@click.option('--config', default='sitic.yml', type=click.Path(exists=True), help="Filesystem path to config file")
+@click.option('--build_draft', is_flag=True, default=False, type=bool, help="Include content marked as draft")
+@click.option('--build_future', is_flag=True, default=False, type=bool, help="Include content with publication_date in the future")
+@click.option('--build_expired', is_flag=True, default=False, type=bool, help="Include expired content")
+@click.option('--disable_sitemap', is_flag=True, default=False, type=bool, help="Do not generate Sitemap")
+@click.option('--disable_rss', is_flag=True, default=False, type=bool, help="Do not generate RSS")
+@click.option('--disable_search', is_flag=True, default=False, type=bool, help="Do not generate search indexer")
+@click.option('--remove_expired', is_flag=True, default=False, type=bool, help="Remove already published content in destination folder")
+@click.option('--clean_destination', is_flag=True, default=False, type=bool, help="Completelty remove destination folder before generation")
 def generate(config, build_draft, build_future, build_expired, disable_sitemap, disable_rss, disable_search, remove_expired, clean_destination):
+    """
+    Generate your site from your contents
+    """
     conf.load_config(config)
     conf.build_draft = build_draft
     conf.build_future = build_future
@@ -59,15 +62,15 @@ def generate(config, build_draft, build_future, build_expired, disable_sitemap, 
     generator.gen()
 
 @cli.command()
-@click.option('--config', default='sitic.yml', type=click.Path(exists=True))
-@click.option('--build_draft', default=False, type=bool)
-@click.option('--build_future', default=False, type=bool)
-@click.option('--build_expired', default=False, type=bool)
-@click.option('--disable_sitemap', is_flag=True, default=False, type=bool)
-@click.option('--disable_rss', is_flag=True, default=False, type=bool)
-@click.option('--disable_search', is_flag=True, default=False, type=bool)
-@click.option('--remove_expired', is_flag=True, default=False, type=bool)
-@click.option('--clean_destination', is_flag=True, default=False, type=bool)
+@click.option('--config', default='sitic.yml', type=click.Path(exists=True), help="Filesystem path to config file")
+@click.option('--build_draft', is_flag=True, default=False, type=bool, help="Include content marked as draft")
+@click.option('--build_future', is_flag=True, default=False, type=bool, help="Include content with publication_date in the future")
+@click.option('--build_expired', is_flag=True, default=False, type=bool, help="Include expired content")
+@click.option('--disable_sitemap', is_flag=True, default=False, type=bool, help="Do not generate Sitemap")
+@click.option('--disable_rss', is_flag=True, default=False, type=bool, help="Do not generate RSS")
+@click.option('--disable_search', is_flag=True, default=False, type=bool, help="Do not generate search indexer")
+@click.option('--remove_expired', is_flag=True, default=False, type=bool, help="Remove already published content in destination folder")
+@click.option('--clean_destination', is_flag=True, default=False, type=bool, help="Completelty remove destination folder before generation")
 @click.option('--port', default=constants.DEFAULT_PORT, type=int)
 def server(config, build_draft, build_future, build_expired, disable_sitemap, disable_rss, disable_search, remove_expired, clean_destination, port):
     """
@@ -92,16 +95,19 @@ def server(config, build_draft, build_future, build_expired, disable_sitemap, di
     server.start()
 
 @cli.command()
-@click.option('--config', default='sitic.yml', type=click.Path(exists=True))
-@click.option('--build_draft', default=False, type=bool)
-@click.option('--build_future', default=False, type=bool)
-@click.option('--build_expired', default=False, type=bool)
-@click.option('--disable_sitemap', is_flag=True, default=False, type=bool)
-@click.option('--disable_rss', is_flag=True, default=False, type=bool)
-@click.option('--disable_search', is_flag=True, default=False, type=bool)
-@click.option('--remove_expired', is_flag=True, default=False, type=bool)
-@click.option('--clean_destination', is_flag=True, default=False, type=bool)
+@click.option('--config', default='sitic.yml', type=click.Path(exists=True), help="Filesystem path to config file")
+@click.option('--build_draft', is_flag=True, default=False, type=bool, help="Include content marked as draft")
+@click.option('--build_future', is_flag=True, default=False, type=bool, help="Include content with publication_date in the future")
+@click.option('--build_expired', is_flag=True, default=False, type=bool, help="Include expired content")
+@click.option('--disable_sitemap', is_flag=True, default=False, type=bool, help="Do not generate Sitemap")
+@click.option('--disable_rss', is_flag=True, default=False, type=bool, help="Do not generate RSS")
+@click.option('--disable_search', is_flag=True, default=False, type=bool, help="Do not generate search indexer")
+@click.option('--remove_expired', is_flag=True, default=False, type=bool, help="Remove already published content in destination folder")
+@click.option('--clean_destination', is_flag=True, default=False, type=bool, help="Completelty remove destination folder before generation")
 def watch(config, build_draft, build_future, build_expired, disable_sitemap, disable_rss, disable_search, remove_expired, clean_destination):
+    """
+    Watch filesystem for changes and recreate as needed
+    """
     conf.load_config(config)
     conf.build_draft = build_draft
     conf.build_future = build_future
@@ -117,6 +123,11 @@ def watch(config, build_draft, build_future, build_expired, disable_sitemap, dis
 @cli.command()
 @click.option('--config', default='sitic.yml', type=click.Path(exists=True))
 def makemessages(config):
+    """
+    Runs over the entire source tree of layouts directory and pulls out all strings marked for translation.
+    It creates (or updates) a message file in the locales directory.
+    After making changes to the messages files you need to compile them with compilemessages for use with the builtin gettext support.
+    """
     conf.load_config(config)
     maker = MakeMessages()
     maker.run()
@@ -124,6 +135,9 @@ def makemessages(config):
 @cli.command()
 @click.option('--config', default='sitic.yml', type=click.Path(exists=True))
 def compilemessages(config):
+    """
+    Compiles .po files created by makemessages to .mo files for use with the built-in gettext support.
+    """
     conf.load_config(config)
     compiler = CompileMessages()
     compiler.compile()
@@ -134,6 +148,9 @@ def compilemessages(config):
 @click.option('--title', default=None, type=str)
 @click.option('--frontmatter', default='toml', type=click.Choice(['toml', 'yaml']))
 def new(filepath, config, title, frontmatter):
+    """
+    Create a new content in the specified path
+    """
     conf.load_config(config)
     new_command = NewCommand(filepath, title, frontmatter)
     new_command.run()
@@ -142,6 +159,9 @@ def new(filepath, config, title, frontmatter):
 @click.argument('filepath')
 @click.option('--config', default='sitic.yml', type=click.Path(exists=True))
 def publish(filepath, config):
+    """
+    Mark specific content as published
+    """
     conf.load_config(config)
     command = PublishCommand(filepath)
     command.run()
@@ -149,5 +169,8 @@ def publish(filepath, config):
 @cli.command()
 @click.argument('path')
 def new_site(path):
+    """
+    Create basic files structure for a sitic project
+    """
     new_site = NewSiteCommand(path)
     new_site.run()
